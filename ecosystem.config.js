cadafe6021c6a82c4075370272da77b50c39f302
Miko Pamintuan <mikopamintuan@gmail.com>
2022-10-23 22:49:32 +0800

    Test deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    deploy: {
+        production: {
+            user: process.env.SSH_USERNAME,
+            host: process.env.SSH_HOST,
+            key: process.env.SSH_PRIVATE_KEY,
+            ref: 'origin/main',
+            repo: 'git@github.com:mikoyats/nugget-bot.git',
+            path: '/home/github/nugget-bot',
+            'post-deploy':
+                'npm install && npm build && pm2 reload ../ecosystem.config.js nugget-bot --env production',
+            env: {
+                NODE_ENV: 'production',
+            },
+        },
+    },
+};
